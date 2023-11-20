7ab66fef39ed49d9b963e1d3bfff9ff8078ae942
terence <terenceshawn18@outlook.com>
2023-11-20 08:45:55 +0300

    added comments to botcollection.js

--- src/components/Botcollection.js
+++ src/components/Botcollection.js
@@ -1,12 +1,16 @@
+
 import React from "react";
 
-function Botcollection({bots,onBotClick}){
-
-    return(
+function Botcollection({ bots, onBotClick }) {
+    // Render the Botcollection component
+    return (
         <>
             <h1>Bot collection </h1>
-           <div id="botCollection">
-           {bots.map((bot) => (
+            {/* Container for the collection of bots */}
+            <div id="botCollection">
+                {/* Map through the array of all bots */}
+                {bots.map((bot) => (
+                    // Render each bot as a card with a click event handler
                     <div key={bot.id} className="bot-card" onClick={() => onBotClick(bot)}>
                         <img src={bot.avatar_url} alt={bot.name} />
                         <p>{bot.name}</p>
@@ -16,9 +20,9 @@ function Botcollection({bots,onBotClick}){
                         <p className="catchphrase">{bot.catchphrase}</p>
                     </div>
                 ))}
-           </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default Botcollection
+export default Botcollection;
